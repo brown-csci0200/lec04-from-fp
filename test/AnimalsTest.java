9cfa3b90935937d742904ec4378d15a3fcc368dc
Nicholas DeMarinis <ndemarinis@wpi.edu>
2024-09-11 13:32:51 -0400

    Fixed test example.

--- test/AnimalsTest.java
+++ test/AnimalsTest.java
@@ -24,7 +24,7 @@ public class AnimalsTest {
     @Test
     public void testExample() {
         // example syntax for assertEquals
-        Assert.assertEquals(1+1, 2);
+        Assert.assertEquals(2, 1 + 1);
     }
     @Test
     public void testMakeDillo() {
